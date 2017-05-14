fe77db3a971cfe94571711a8068e7e284873b41d
MY-PC\yukiT <g1527047@st.t-kougei.ac.jp>
2017-05-15 00:07:28 +0900

    文字列判定用関数追加・実装

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <string>
 #include "Calculator.h"
+
+#define MAX_DIGIT 50
+#define END_STRING "0"
+
+using namespace std;
 
 void Calculator :: Run()
 {
-	std::cout << "HEllO";
+	for(;;)
+	{
+		Calculator::Input();
+
+		//—LŒø‚È“ü—Í‚Å‚ ‚é‚©‚ðŠm”F
+		if (Calculator::CheckEnd())
+			break;
+		if (Calculator::CheckOverString())
+			continue;
+		if (Calculator::HasString())
+			continue;
+	}
+}
+
+void Calculator :: Input()
+{
+	cout << "“ü—Í„„";
+
+	cin >> input;
+}
+
+bool Calculator::CheckEnd()
+{
+	if (input == END_STRING)
+		return true;
+	else
+		return false;
+}
+
+bool Calculator::CheckOverString()
+{
+	if (input.length() > MAX_DIGIT)
+		return true;
+	else
+		return false;
+}
+
+bool Calculator::HasString()
+{
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] < '0' || input[i] > '9')
+			return true;
+	}
+
+	return false;
 }
--- Calculator/Calculator/Calculator.h
+++ Calculator/Calculator/Calculator.h
@@ -1,11 +1,21 @@
 #ifndef ___Class_Calculator
 #define ___Class_Calculator
+#include <iostream>
+#include <string>
 
 class Calculator
 {
 public:
 
 	void Run();
+
+private:
+	std :: string input;
+
+	void Input();
+	bool CheckEnd();
+	bool CheckOverString();
+	bool HasString();
 };
 
 #endif
